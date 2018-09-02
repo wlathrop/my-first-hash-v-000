6969af6825e35348570e1d6b6f96de65a8d18bd7
William Lathrop <wlathrop10@gmail.com>
2018-09-02 00:10:24 +0000

    automatically backed up by learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -27,7 +27,9 @@ def adding
   shipping_manifest = {
                   "whale bone corsets" => 5, 
                   "porcelain vases" => 2, 
-                  "oil paintings" => 3
+                  "oil paintings" => 3,
+                  "muskets" => 2,
+                  "gun powder" => 4
                  }
   # add 2 muskets to the shipping_manifest hash below
 
